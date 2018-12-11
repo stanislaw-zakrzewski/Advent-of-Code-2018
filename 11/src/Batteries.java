b8cb904519fc6789c65a95ad310c92d4ba2b9a87
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-12-11 12:53:12 +0100

    Day 11 completed

--- 11/src/Batteries.java
+++ 11/src/Batteries.java
@@ -1,28 +1,27 @@
-public class Batteries {
+class Batteries {
     private int[][] grid;
 
-    public Batteries(int gridSerialNumber) {
+    Batteries(int gridSerialNumber) {
         grid = new int[300][300];
         for (int i = 0; i < 300; i++) {
             for (int j = 0; j < 300; j++) {
-                int pom = j + 1 + 10;
-                pom *= (i + 1);
-                pom += 18;
-                pom = ((pom % 1000) * (pom % 1000)) % 1000;
+                int pom = i + 1 + 10;
+                pom *= (j + 1);
+                pom += gridSerialNumber;
+                pom = ((pom % 1000) * ((i + 1 + 10) % 1000)) % 1000;
                 pom = pom - pom % 100;
                 pom /= 100;
                 pom -= 5;
-                grid[j][i] = pom;
+                grid[i][j] = pom;
             }
         }
-        System.out.println(calculateValue(32,44));
     }
 
-    public int[] findBest() {
+    int[] findBest3by3() {
         int[] best = new int[]{0, 0};
         for (int i = 0; i < 288; i++) {
             for (int j = 0; j < 288; j++) {
-                if (calculateValue(i, j) > calculateValue(best[0], best[1])) {
+                if (calculateValue(i, j, 3) > calculateValue(best[0], best[1], 3)) {
                     best[0] = i;
                     best[1] = j;
                 }
@@ -33,10 +32,31 @@ public class Batteries {
         return best;
     }
 
-    private int calculateValue(int x, int y) {
+    int[] findBestAnySize() {
+        int[] best = new int[]{0, 0, 0};
+        int bestValue = 0;
+        for (int s = 1; s < 300; s++) {
+            for (int i = 0; i < 300 - s; i++) {
+                for (int j = 0; j < 300 - s; j++) {
+                    int nextValue = calculateValue(i, j, s);
+                    if (nextValue > bestValue) {
+                        bestValue = nextValue;
+                        best[0] = i;
+                        best[1] = j;
+                        best[2] = s;
+                    }
+                }
+            }
+        }
+        best[0]++;
+        best[1]++;
+        return best;
+    }
+
+    private int calculateValue(int x, int y, int size) {
         int ret = 0;
-        for (int i = x; i < x + 3; i++) {
-            for (int j = y; j < y + 3; j++) {
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
                 ret += grid[i][j];
             }
         }
--- 11/src/Main.java
+++ 11/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
-    private static final int gridSerialNumber = 18;
+    private static final int gridSerialNumber = 7857;
 
     public static void main(String[] args) {
         Batteries batteries = new Batteries(gridSerialNumber);
-        int[] best = batteries.findBest();
+        int[] best = batteries.findBest3by3();
         System.out.println("1: " + best[0] + "," + best[1]);
+        int[] bestAny = batteries.findBestAnySize();
+        System.out.println("2: " + bestAny[0] + "," + bestAny[1] + "," + bestAny[2]);
     }
 }
